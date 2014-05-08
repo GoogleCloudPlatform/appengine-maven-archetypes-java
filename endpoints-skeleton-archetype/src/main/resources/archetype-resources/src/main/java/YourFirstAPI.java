6c671873b494204cd43f0987368421e37f46560b
Takashi Matsuo <tmatsuo@google.com>
2014-05-08 15:03:56 -0700

    Added endpoints-skeleton-archetype.
    Reviewed at: https://codereview.appspot.com/98110043/

---
+++ endpoints-skeleton-archetype/src/main/resources/archetype-resources/src/main/java/YourFirstAPI.java
@@ -0,0 +1,11 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
