8b53cb61f9ed0ed8a49de0c22f54e496332082ad
Elliotte Rusty Harold <elharo@google.com>
2016-06-04 12:00:32 -0700

    @Override

--- appengine-skeleton-archetype/src/main/resources/archetype-resources/src/main/java/HelloAppEngine.java
+++ appengine-skeleton-archetype/src/main/resources/archetype-resources/src/main/java/HelloAppEngine.java
@@ -27,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloAppEngine extends HttpServlet {
 
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
       
