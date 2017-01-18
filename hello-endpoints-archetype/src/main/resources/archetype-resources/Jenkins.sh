496f2f29565caf76b004778dd37b8142a4f4cce3
Frank Natividad <franknatividad@google.com>
2017-01-18 10:22:26 -0800

    Updating Jenkins.sh to fail on non-zero return

--- hello-endpoints-archetype/src/main/resources/archetype-resources/Jenkins.sh
+++ hello-endpoints-archetype/src/main/resources/archetype-resources/Jenkins.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+# Fail on non-zero return
+set -e
+
 # Jenkins Test Script
 # Test getGreeting Endpoint (hello world!)
 curl -X GET \
   "https://${GOOGLE_PROJECT_ID}.appspot.com/_ah/api/helloworld/v1/hellogreeting/0" | \
-  tee /dev/tty | grep "hello world!"
+  grep "hello world!"
 
 # Test getGreeting Endpoint (goodbye world!)
 curl -X GET \
   "https://${GOOGLE_PROJECT_ID}.appspot.com/_ah/api/helloworld/v1/hellogreeting/1" | \
-  tee /dev/tty | grep "goodbye world!"
+  grep "goodbye world!"
 
 # Test listGreeting Endpoint (hello world! and goodbye world!)
 curl -X GET \
   "https://${GOOGLE_PROJECT_ID}.appspot.com/_ah/api/helloworld/v1/hellogreeting" | \
-  tee /dev/tty | grep "hello world!\|goodbye world!"
+  grep "hello world!\|goodbye world!"
 
 # Test multiply Endpoint (This is a greeting.)
 curl -X POST \
   -H "Content-Type: application/json" \
   --data "{'message':'This is a greeting.'}" \
   "https://${GOOGLE_PROJECT_ID}.appspot.com/_ah/api/helloworld/v1/hellogreeting/1" | \
-  tee /dev/tty | grep "This is a greeting."
+  grep "This is a greeting."
